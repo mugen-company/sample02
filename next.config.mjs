3e381434bca2199c2d948cf03a7d26485c26eccc
yamamoto <d.yamamoto.amakusa.nakamuraya@gmail.com>
2024-09-10 16:16:24 +0900

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/sample01', // リポジトリ名
+    assetPrefix: '/sample01', // リポジトリ名
+    trailingSlash: true, // URL の末尾にスラッシュを追加
+};
 
 export default nextConfig;
