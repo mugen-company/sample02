0a8241800c763b05d5c01e5060de7eb883e9e34b
yamamoto <d.yamamoto.amakusa.nakamuraya@gmail.com>
2024-09-10 17:04:04 +0900

    ss

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     basePath: 'https://github.com/mugen-company/sample01.git', // リポジトリ名
     assetPrefix: 'https://github.com/mugen-company/sample01.git', // リポジトリ名
     trailingSlash: true, // URL の末尾にスラッシュを追加
+    output: "export",
 };
 
 export default nextConfig;
