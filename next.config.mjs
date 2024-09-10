aca9e50fd3ed74bb6290399e2119351d4949631e
yamamoto <d.yamamoto.amakusa.nakamuraya@gmail.com>
2024-09-10 18:40:51 +0900

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/sample01.git', // リポジトリ名
-    assetPrefix: '/sample01.git', // リポジトリ名
-    trailingSlash: true, // URL の末尾にスラッシュを追加
+    // basePath: '/sample01.git', // リポジトリ名
+    // assetPrefix: '/sample01.git', // リポジトリ名
+    // trailingSlash: true, // URL の末尾にスラッシュを追加
     output: "export",
 };
 
