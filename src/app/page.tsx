d166ff46641a1c34992b8f758f5ebabfe0cc6ee6
yamamoto <d.yamamoto.amakusa.nakamuraya@gmail.com>
2024-09-11 16:57:37 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import AnimatedLink from '../../component/AnimatedLink';
+import AnimatedLink from "../../component/AnimatedLink";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -13,7 +14,98 @@ export default function Home() {
             <AnimatedLink href="/services">Services</AnimatedLink>
           </ul>
         </header>
-        <footer>@copyright sample</footer>
+        <footer>
+          <motion.p
+            initial={{ opacity: 0, y: 40 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            @
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 50 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            c
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 60 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            o
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 70 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            p
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 80 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            y
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 90 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            r
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 100 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            i
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 110 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            g
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 120 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            h
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 130 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            t
+          </motion.p>
+        </footer>
       </main>
     </div>
   );
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import AnimatedLink from '../../../component/AnimatedLink';
+import {motion} from 'framer-motion'
 
 export default function Services() {
   return (
@@ -13,6 +14,99 @@ export default function Services() {
             <AnimatedLink href="/contact">Contact</AnimatedLink>
           </ul>
         </header>
+
+        <footer>
+          <motion.p
+            initial={{ opacity: 0, y: 40 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            @
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 50 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            c
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 60 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            o
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 70 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            p
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 80 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            y
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 90 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            r
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 100 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            i
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 110 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            g
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 120 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            h
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 130 }} // 初期状態
+            animate={{ opacity: 1, y: 0 }} // アニメーション終了後の状態
+            transition={{ duration: 1.9 }} // アニメーションの持続時間
+            whileHover={{ scale: 1.05 }} // ホバー時に少し拡大>
+            
+          >
+            t
+          </motion.p>
+        </footer>
       </main>
     </div>
   );
