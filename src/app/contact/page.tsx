b2461a107bfafe26de5ed601980641abd7eb2512
mugen-company <d.yamamoto@amakusa-nakamuraya.com>
2024-09-18 14:04:15 +0900

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ import ImageSlider from '../../../component/ImageSlider';
 
 export default function Contact() {
 
-  const images = ["/images/kv_1.jpg", "/images/kv_3.jpg", "/images/kv_1.jpg"]; // スライドショー用の画像パス
+  const images = ["https://github.com/mugen-company/sample01/blob/main/public/images/kv_1.jpg?raw=true", "https://github.com/mugen-company/sample01/blob/main/public/images/kv_3.jpg?raw=true", "https://github.com/mugen-company/sample01/blob/main/public/images/kv_1.jpg?raw=true"]; // スライドショー用の画像パス
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
