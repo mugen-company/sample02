f5f0a3fee3b62d22c06f17b5be213eeb1510989d
mugen-company <d.yamamoto@amakusa-nakamuraya.com>
2024-09-18 14:01:34 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Head from "../../component/Head";
 import ImageSlider from "../../component/ImageSlider";
 
 export default function Home() {
-  const images = ["/images/kv_1.jpg", "/images/kv_3.jpg", "/images/kv_1.jpg"]; // スライドショー用の画像パス
+  const images = ["https://github.com/mugen-company/sample01/blob/main/public/images/kv_1.jpg?raw=true", "/images/kv_3.jpg", "https://github.com/mugen-company/sample01/blob/main/public/images/kv_1.jpg?raw=true"]; // スライドショー用の画像パス
 
   const [isClient, setIsClient] = useState(false);
 
